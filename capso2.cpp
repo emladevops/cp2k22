b75463abe30a48a52b79a9c3c7b60cf0984c1f59
Tran Trong Nghia <tran1903@tran1903.dev>
2022-11-02 19:32:42 +0700

    2/11

--- capso2.cpp
+++ capso2.cpp
@@ -1,22 +1,18 @@
-#include<iostream>
-
-using namespace std;
-typedef long ll;
+#include<stdio.h>
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    ll a, b, c, d, count = 0;
-    cin >> a >> b >> c >> d;
-    for(int x = 0; x <= d; x++) {
-        for(int y = 0; y <= d; y++) {
-            if (x <= y && a <= x*y && x*y <=b && c <= 2*(x+y) && 2*(x+y) <= d) {
-                count++;
+    long long int a, b, c, d, count = 0;
+    scanf("%lld%lld%lld%lld", &a, &b, &c, &d);
+    for(long long x = 0; x <= d; x++) {
+        for(long long y = 0; y <= d; y++) {
+            if (x <= y && a <= x*y) {
+                if (x*y <=b && c <= 2*(x+y) && 2*(x+y) <= d) {
+                    count = count + 1;
+                }
             }
         }
     }
-    cin.tie(0);
-    cout.tie(0);
-    cout << count;
+    printf("%lld", count);
 
     return 0;
 }

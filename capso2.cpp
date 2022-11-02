1e186e0c452f09639fb2db7c4c708efd66679fb0
Tran Trong Nghia <tran1903@tran1903.dev>
2022-11-02 20:43:03 +0700

    fix

--- capso2.cpp
+++ capso2.cpp
@@ -1,18 +1,24 @@
 #include<stdio.h>
 
+int max(long long a, long long b, long long c, long long d) {
+    long long max = a;
+    if (b > max) max = b;
+    if (c > max) max = c;
+    if (d > max) max = d;
+    return max;
+}
+
 int main() {
-    long long int a, b, c, d, count = 0;
+    long long a, b, c, d, count = 0;
     scanf("%lld%lld%lld%lld", &a, &b, &c, &d);
-    for(long long x = 0; x <= d; x++) {
-        for(long long y = 0; y <= d; y++) {
-            if (x <= y && a <= x*y) {
-                if (x*y <=b && c <= 2*(x+y) && 2*(x+y) <= d) {
-                    count = count + 1;
-                }
+    long long g = max(a, b, c, d);
+    for(long long x = 0; x <= g; x++) {
+        for(long long y = 0; y <= g; y++) {
+            if(x <= y && a<=x*y && x*y<=b && c<=2*(x+y) && 2*(x+y)<=d) {
+                count++;
             }
         }
     }
     printf("%lld", count);
-
     return 0;
 }
